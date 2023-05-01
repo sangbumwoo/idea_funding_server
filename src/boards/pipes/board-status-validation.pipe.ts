79805de0c247041d61d38e4b2184f55b765c6979
SagbumWoo <sbwoo87@gmail.com>
2023-05-01 20:16:44 +0900

    add boards

---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,22 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from "@nestjs/common";
+import { BoardStatus } from "../board.model";
+
+export class BoardStatusValidationPipe implements PipeTransform {
+    readonly BoardSatus = [
+        BoardStatus.PRIVATE,
+        BoardStatus.PUBLIC
+    ];
+    transform(value: any, metadata: ArgumentMetadata) {
+        value = value.toUpperCase();
+        if (!this.isStatusValid(value)) {
+            throw new BadRequestException(`${value} isn't valid status options.`); 
+        }
+        return value;
+    }
+
+    private isStatusValid(value: any) {
+        const index = this.BoardSatus.indexOf(value);
+        return index !== -1;
+    }
+
+}
